ce83a7a3606f0b526883b8ac46838834e8d51a72
odon <odon.lozada@axity.com>
2022-10-26 13:04:22 -0500

    login

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
   exports: [LoginComponent, RegisterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, FormsModule],
 })
 export class AuthModule {}
